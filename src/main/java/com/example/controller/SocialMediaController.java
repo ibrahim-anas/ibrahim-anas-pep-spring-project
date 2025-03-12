4875618589d0129241f13eb1f5ca0dd290422d8f
Anas Ibrahim <ibrahim-anas@users.noreply.github.com>
2025-03-12 19:46:11 +0000

    Add necessary annotations

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,5 +1,13 @@
 package com.example.controller;
 
+import com.example.entity.Account;
+import com.example.service.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.service.MessageService;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
@@ -7,6 +15,19 @@ package com.example.controller;
  * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
+@RestController
 public class SocialMediaController {
 
+    @Autowired
+    private AccountService AccountService;
+
+    @Autowired
+    private MessageService messageService;
+
+    @PostMapping("/register")
+    public ResponseEntity<Account> userRegistration() {
+        return null;
+    }
+
+    
 }
--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,14 @@
 package com.example.service;
 
+import org.springframework.stereotype.Service;
+
+import com.example.repository.AccountRepository;
+
+@Service
 public class AccountService {
+    private AccountRepository accountRepository;
+
+    public AccountService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
 }
